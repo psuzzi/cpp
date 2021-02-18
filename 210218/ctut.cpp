51323bec5078b3598498ff8c02fd2f0ef717fd2b
Patrik Suzzi <psuzzi@gmail.com>
2021-02-18 23:25:42 +0100

    cpp tut start

---
+++ 210218/ctut.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+    cout << "Hello World" << endl;
+
+    const double PI = 3.14159;
+
+    char myGrade = 'A';
+
+    bool isH = true;
+
+    int age = 42;
+
+    float favNum = 3.14159;
+
+
+    cout << "Favorite num. " << favNum << endl;
+
+
+    /* code */
+    return 0;
+}
---
+++ 210218/readme.md
@@ -0,0 +1,7 @@
+#
+
+    # compile to .out file
+    g++ -std=c++11 ctut.cpp
+
+    # run
+    ./a.out

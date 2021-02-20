d71902eea6b5a45f14d6a61d4e07275733bb5cd6
Patrik Suzzi <psuzzi@gmail.com>
2021-02-20 16:37:46 +0100

    change main input and gitignore

--- lcode/771-jewels-and-stones/main.cpp
+++ lcode/771-jewels-and-stones/main.cpp
@@ -3,28 +3,54 @@
 
 using namespace std;
 
+struct Input
+{
+    string a;
+    string b;
+};
+
+struct Test
+{
+    int t;
+    vector<Input> in;
+    vector<int> exp;
+};
+
+// operator to read input
+std::istream &operator>>(std::istream &is, Test &test)
+{
+    is >> test.t;
+    for(int i=0; i<test.t; i++){
+        Input in;
+        int ans;
+        is >> in.a >> in.b;
+        is >> ans;
+        test.in.push_back(in);
+        test.exp.push_back(ans);
+    }
+    return is;
+};
+
 int main(int argc, char const *argv[])
 {
     Solution sol;
 
-    std:ifstream infile("in.txt");
-    if(!infile.is_open()){
+    std:ifstream inputfile("in.txt");
+    if(!inputfile){
         cout << "Error reading input file" << endl;
         return 1;
     }
 
-    // 1st line # of test cases
-    int t, n=0;
-    infile >> t;
-    string a, b;
-    int e;
-    while( n++ < t ){
-        infile >> a >> b;
-        infile >> e;
-        int ans = sol.numJewelsInStones(a, b);
-        cout << "Answer" << n << " : " << ans << " " << ((ans == e) ? "Correct" : "Wrong ") << endl;
+    Test test;
+    inputfile >> test;
+
+    for(int i=0; i<test.t; i++){
+        int ans = sol.numJewelsInStones(test.in[i].a, test.in[i].b);
+        int e = test.exp[i];
+        cout << "Answer" << (i+1) << " : " << ans << " " << ((ans == e) ? "Correct" : "Wrong ") << endl;
     }
 
     /* code */
     return 0;
-}
+};
+

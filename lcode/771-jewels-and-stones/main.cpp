36e1ffe1b5e3fc6602f6f3e0505fd3ead802891e
Patrik Suzzi <psuzzi@gmail.com>
2021-02-20 15:25:13 +0100

    add standard header

--- lcode/771-jewels-and-stones/main.cpp
+++ lcode/771-jewels-and-stones/main.cpp
@@ -1,7 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <fstream>
+#include "../StandardHeaders.hpp"
 #include "Solution.cpp"
 
 using namespace std;
---
+++ lcode/StandardHeaders.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <set>
+#include <vector>
+#include <limits>
+#include <algorithm>
+#include <array>
+#include <iterator>
+#include <string>
+#include <cassert>
+#include <exception>
+#include <deque>
+#include <memory>
+#include <future>
+#include <thread>
+#include <chrono>
+#include <limits>
+#include <random>
+

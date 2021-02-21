5c4e89b209384db363493fb8f925e4a58d41a392
Patrik Suzzi <psuzzi@gmail.com>
2021-02-21 02:23:59 +0100

    Add aexpert readme and header

---
+++ aexpert/StandardHeaders.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <set>
+#include <vector>
+#include <limits>
+#include <algorithm>
+#include <array>
+#include <iterator>
+#include <string>
+#include <cassert>
+#include <exception>
+#include <deque>
+#include <memory>
+#include <future>
+#include <thread>
+#include <chrono>
+#include <limits>
+#include <random>
+#include <unordered_set>
+
---
+++ aexpert/gen.py
@@ -0,0 +1,25 @@
+
+#!/usr/local/bin/python3
+
+import os
+from os import path
+import glob
+import urllib
+
+dlist = []
+for name in glob.glob('[0-9]*-*'):
+    dlist.append(name)
+
+if dlist:
+    # rewrite readme
+    f = open("readme.md", "w")
+    f.write("# List of exercises\n\n")
+    
+    for n in dlist:
+        num, name = n.split('-', 1)
+        url = urllib.quote( 'www.algoexpert.io/questions/{}/'.format(name) ) 
+        f.write(' * [#{} : {}](<{}>) - ([algoexpert]({})) \n'.format(num, name, n, url))
+
+    f.write("\n")
+    f.close()
+

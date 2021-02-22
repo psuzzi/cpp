3cf33c103e2af86c04816886883394b82e43634b
Patrik Suzzi <psuzzi@gmail.com>
2021-02-22 20:40:01 +0100

    added lc 200, number of islands

---
+++ lcode/200-number-of-islands/Solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    
+public:
+    int numIslands(vector<vector<char>>& grid) 
+    {
+        int count = 0;
+        
+        for( int i=0; i<grid.size(); i++)
+        {
+            for(int j=0; j<grid[i].size(); j++)
+            {
+                if( grid[i][j] == '1' )
+                {
+                    dfs(i, j, grid);
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+
+    void dfs(int i, int j, vector<vector<char>>& grid)
+    {
+        if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size() || grid[i][j]=='0')
+            return;
+        grid[i][j] = '0';
+        dfs(i, j+1, grid);
+        dfs(i, j-1, grid);
+        dfs(i-1, j, grid);
+        dfs(i+1, j, grid);
+        return;
+    }
+};
